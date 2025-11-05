38070585000b0c232752389a044b31c71c0d920c
Breyner-Valenciano <for.the.worthy34@gmail.com>
2025-11-05 17:52:11 -0500

    Add files via upload

---
+++ ejercicios phyton/ejercicios phyton/circulo.py
@@ -0,0 +1,8 @@
+import math
+
+r = int(input("definimos el valor de r: "))
+s = math.pi * r**2
+p = 2 * math.pi * r
+
+print("su superficie es: ", s)
+print("su perimetro es: ", p)
---
+++ ejercicios phyton/ejercicios phyton/salario.py
@@ -0,0 +1,4 @@
+numHoras = int(input("Ingrese el numero de horas laboradas: "))
+valHoras = int(input("Ingrese el valor de la hora: "))
+salario = numHoras * valHoras
+print("su salario corresponde a: ", salario)
