0a5ef68b45210bdc52f8a5a389f849712e56bf35
Breyner-Valenciano <for.the.worthy34@gmail.com>
2025-11-07 12:21:06 -0500

    Delete ejercicios phyton/phyton.py

--- ejercicios phyton/phyton.py
+++
@@ -1,9 +0,0 @@
-lenguaje = "phyton"
-lenguaje = "java"
-lenguaje = "c++"
-if lenguaje == "phyton":
-    print("estoy de acuerdo, phyton es el mejor")
-elif lenguaje == "java":
-    print("no me gusta, java no mola")
-else:
-    print("ningun otro lenguaje supera a java")
