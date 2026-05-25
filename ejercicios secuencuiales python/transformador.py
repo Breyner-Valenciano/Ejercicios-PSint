37676ee345fe7ce1f4f35895707d90c3b7ba86b2
Breyner-Valenciano <for.the.worthy34@gmail.com>
2026-05-25 09:52:01 -0500

    Add files via upload

---
+++ ejercicios secuencuiales python/transformador.py
@@ -0,0 +1,12 @@
+a = float(input("Inserte su valor en centímetros: "))
+
+yardas = a / 91.44
+metros = a / 100
+pies = a / 30.48
+pulgadas = a / 2.54
+
+print("Su valor:", a, "cm")
+print("En yardas es:", yardas, "yd")
+print("En metros es:", metros, "m")
+print("En pies es:", pies, "ft")
+print("En pulgadas es:", pulgadas, "in")
